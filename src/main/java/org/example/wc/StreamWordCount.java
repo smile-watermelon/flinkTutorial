076a1d57d61c0c6d3a3ef2a87f45dbb034e27a33
smile watermelon <18516833610@163.com>
2022-05-26 19:47:53 +0800

    unbounded stream word count use program parameters

--- src/main/java/org/example/wc/StreamWordCount.java
+++ src/main/java/org/example/wc/StreamWordCount.java
@@ -2,6 +2,7 @@ package org.example.wc;
 
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -16,8 +17,13 @@ public class StreamWordCount {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        //        获取参数
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String hostname = parameterTool.get("hostname");
+        int port = parameterTool.getInt("port");
 //        2、读取文本流
-        DataStreamSource<String> source = env.socketTextStream("localhost", 9999);
+        DataStreamSource<String> source = env.socketTextStream(hostname, port);
 
         SingleOutputStreamOperator<Tuple2<String, Integer>> op = source.flatMap((String line, Collector<Tuple2<String, Integer>> out) -> {
             String[] words = line.split(" ");
